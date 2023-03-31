6206505c3482f6de8e45db024433cf3102f8f624
sumanshil <technicalblog.shil@gmail.com>
2023-03-31 09:27:23 -0700

    Read from stdin

--- week-1/word_count/ccwc.go
+++ week-1/word_count/ccwc.go
@@ -2,37 +2,77 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 	"unicode/utf8"
 )
 
 func main() {
-	optionToHandlerFunc := make(map[string]func(string2 string) (int, error))
-	optionToHandlerFunc["-c"] = getFileSize
+
+	optionToHandlerFunc := make(map[string]func(data string) (int, error))
 	optionToHandlerFunc["-l"] = getLineCount
 	optionToHandlerFunc["-w"] = getWordCount
-	optionToHandlerFunc["-n"] = getCharacterCount
+	optionToHandlerFunc["-m"] = getCharacterCount
+	stat, _ := os.Stdin.Stat()
 	results := make([]interface{}, 0)
-	if len(os.Args) == 3 {
-		option := os.Args[1]
-		file := os.Args[2]
-		i, err := optionToHandlerFunc[option](file)
-		if err != nil {
-			return
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		stdin, _ := io.ReadAll(os.Stdin)
+		lines := string(stdin)
+		if len(os.Args) == 2 {
+			option := os.Args[1]
+			result, _ := optionToHandlerFunc[option](lines)
+			results = append(results, result)
+		} else {
+			for _, function := range optionToHandlerFunc {
+				i, err := function(lines)
+				if err != nil {
+					return
+				}
+				results = append(results, i)
+			}
 		}
-		results = append(results, int(i))
-		results = append(results, file)
-	} else if len(os.Args) == 2 {
-		file := os.Args[1]
-		for _, function := range optionToHandlerFunc {
-			i, err := function(file)
+	} else {
+		if len(os.Args) == 3 {
+			option := os.Args[1]
+			file := os.Args[2]
+			if option == "-c" {
+				i, _ := getFileSize(file)
+				results = append(results, i)
+			} else {
+				data, err := os.ReadFile(file)
+				if err != nil {
+					return
+				}
+
+				i, err := optionToHandlerFunc[option](string(data))
+				if err != nil {
+					return
+				}
+				results = append(results, int(i))
+			}
+			results = append(results, file)
+		} else if len(os.Args) == 2 {
+			file := os.Args[1]
+			data, err := os.ReadFile(file)
 			if err != nil {
 				return
 			}
-			results = append(results, i)
+			//for _, function := range optionToHandlerFunc {
+			//	i, err := function(string(data))
+			//	if err != nil {
+			//		return
+			//	}
+			//	results = append(results, i)
+			//}
+			r, _ := getLineCount(string(data))
+			results = append(results, r)
+			r, _ = getWordCount(string(data))
+			results = append(results, r)
+			r, _ = getFileSize(file)
+			results = append(results, r)
+			results = append(results, file)
 		}
-		results = append(results, file)
 	}
 	for _, val := range results {
 		if _, ok := val.(string); ok {
@@ -55,32 +95,17 @@ func getFileSize(filename string) (int, error) {
 	return int(size), nil
 }
 
-func getLineCount(filename string) (int, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return -1, err
-	}
-	//fmt.Printf(string(data))
-	temp := strings.Split(string(data), "\n")
+func getLineCount(data string) (int, error) {
+	temp := strings.Split(data, "\n")
 	return len(temp), nil
 }
 
-func getWordCount(filename string) (int, error) {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return -1, err
-	}
-	temp := strings.Fields(string(data))
+func getWordCount(data string) (int, error) {
+	temp := strings.Fields(data)
 	return len(temp), nil
 }
 
-func getCharacterCount(filename string) (int, error) {
-	data, err := os.ReadFile(filename)
-
-	if err != nil {
-		return -1, err
-	}
-	//fmt.Printf(string(data))
-	temp := utf8.RuneCountInString(string(data))
+func getCharacterCount(data string) (int, error) {
+	temp := utf8.RuneCountInString(data)
 	return temp, nil
 }
